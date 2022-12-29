2e00ac351e49a432d06240b0dd5dbaee00526d17
Marina Nogovitsyna <97659223+MariNogo@users.noreply.github.com>
2022-12-29 10:22:56 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,80 @@
+username = 'janitta'
+password = 'asdfghjkl'
+
+# Создание нового пользователя
+created_user = {
+  "id": 0,
+  "username": "Janitta",
+  "firstName": "Яна",
+  "lastName": "Борисова",
+  "email": "love_sick@bk.ru",
+  "password": "asdfghjkl",
+  "phone": "+79236786545",
+  "userStatus": 0
+}
+
+# Обновление данных пользователя
+updated_user = {
+  "id": 0,
+  "username": "Janitta2",
+  "firstName": "Яна2",
+  "lastName": "Борисова2",
+  "email": "love_sick2@bk.ru",
+  "password": "asdfgh2345672",
+  "phone": "79236786546",
+  "userStatus": 0
+}
+
+# Создание списка пользователей
+list_of_users = [
+  {
+    "id": 0,
+    "username": "Janitta3",
+    "firstName": "Яна3",
+    "lastName": "Борисова3",
+    "email": "love_sick2@bk.ru3",
+    "password": "asdfgh2345673",
+    "phone": "+79236786547",
+    "userStatus": 0
+  },
+  {
+    "id": 0,
+    "username": "Janitta4",
+    "firstName": "Яна4",
+    "lastName": "Борисова4",
+    "email": "love_sick2@bk.ru4",
+    "password": "asdfgh2345674",
+    "phone": "+792367865478",
+    "userStatus": 0
+  }
+]
+
+# Создание резерва
+order = {
+  "id": 0,
+  "petId": 0,
+  "quantity": 0,
+  "shipDate": "0",
+  "status": "placed",
+  "complete": True
+}
+
+# Добавление нового питомца
+new_pet = {
+  "id": 0,
+  "category": {
+    "id": 0,
+    "name": "Category"
+  },
+  "name": "Name",
+  "photoUrls": [
+    "None"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "Tags"
+    }
+  ],
+  "status": "available"
+}
